5213d7cc5c2a48cfb6129ef8b1c23e22b4986d53
kikitaleino <devkinitaleino@gmail.com>
2025-12-04 21:38:39 +0500

    data preparation step is complete

--- scripts/data_preparation.py
+++ scripts/data_preparation.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import pandas as pd
+import csv
 from config import config
 
 def exec_mmseqs_easy_cluster(
@@ -17,6 +19,40 @@ def exec_mmseqs_easy_cluster(
                    shell=True,
                    text=True)
 
+def filter_redundant_seq(
+    clustered_tsv_path,
+    seq_tsv_path,
+    save_path,
+):
+    """
+    filter_redundant_seq creates a dataframe with only representative sequences
+    :param clustered_tsv_path: path to tsv with clustered sequences
+    :param seq_tsv_path: path to tsv with sequences
+    :param save_path: path to save filtered tsv
+    """
+    clusters_df = pd.read_csv(clustered_tsv_path, sep='\t', names=['cluster', 'sequence'])
+    reps = clusters_df['cluster'].unique()
+
+    df = pd.read_csv(seq_tsv_path, sep='\t', names=['ID', 'Taxa', 'Kingdom', 'Seq_Length', 'Site'])
+
+    filtered = df.loc[df['ID'].isin(reps)]
+    filtered.to_csv(save_path, sep='\t', quoting=csv.QUOTE_NONE)
+
+    return filtered
+
+def train_test_split(df):
+    i = int(len(df) * 0.8)
+
+    train = df.iloc[0:i]
+    test = df.iloc[i:]
+
+    return train, test
+
+def prepare_cross_validation_set(df):
+    l = [0, int(len(df)*0.2), int(len(df)*0.4), int(len(df)*0.6), int(len(df)*0.8), len(df)]
+
+    return [df.iloc[l[n]:l[n + 1]] for n in range(len(l) - 1)]
+
 if __name__ == "__main__":
     os.makedirs(config.config["data_preparation_dir"], exist_ok=True)
 
@@ -28,6 +64,29 @@ if __name__ == "__main__":
         config.config["coverage_threshold"],
     )
 
+    pos_filtered = filter_redundant_seq(
+        f"{config.config["data_preparation_dir"]}/{config.config["positive_prefix"]}/cluster-results_cluster.tsv",
+        f"{config.config["data_collection_dir"]}/{config.config["positive_prefix"]}.tsv",
+        f"{config.config["data_preparation_dir"]}/{config.config["positive_prefix"]}_filtered.tsv"
+    )
+
+    pos_train, pos_test = train_test_split(pos_filtered)
+    pos_test.to_csv(
+        f"{config.config["data_preparation_dir"]}/{config.config["positive_prefix"]}_test.tsv",
+        sep='\t',
+        quoting=csv.QUOTE_NONE,
+    )
+
+    validation_sets = prepare_cross_validation_set(pos_train)
+    print(len(validation_sets))
+    for i in range(len(validation_sets)):
+        print(validation_sets[i])
+        validation_sets[i].to_csv(
+            f"{config.config["data_preparation_dir"]}/{config.config["positive_prefix"]}_train_{i}.tsv",
+            sep='\t',
+            quoting=csv.QUOTE_NONE,
+        )
+
     exec_mmseqs_easy_cluster(
         config.config["data_collection_dir_inside_output"],
         config.config["data_preparation_dir"],
@@ -35,3 +94,24 @@ if __name__ == "__main__":
         config.config["min_seq_id"],
         config.config["coverage_threshold"],
     )
+
+    neg_filtered = filter_redundant_seq(
+        f"{config.config["data_preparation_dir"]}/{config.config["negative_prefix"]}/cluster-results_cluster.tsv",
+        f"{config.config["data_collection_dir"]}/{config.config["negative_prefix"]}.tsv",
+        f"{config.config["data_preparation_dir"]}/{config.config["negative_prefix"]}_filtered.tsv"
+    )
+
+    neg_train, neg_test = train_test_split(neg_filtered)
+    neg_test.to_csv(
+        f"{config.config["data_preparation_dir"]}/{config.config["negative_prefix"]}_test.tsv",
+        sep='\t',
+        quoting=csv.QUOTE_NONE,
+    )
+
+    validation_sets = prepare_cross_validation_set(neg_train)
+    for i in range(len(validation_sets)):
+        validation_sets[i].to_csv(
+            f"{config.config["data_preparation_dir"]}/{config.config["negative_prefix"]}_train_{i}.tsv",
+            sep='\t',
+            quoting=csv.QUOTE_NONE,
+        )
